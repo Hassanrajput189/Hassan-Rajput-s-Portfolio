ea3b0dfda67043fde5e146d954558542a4c1e123
Hassan Rajput <hassanrajput189@gmail.com>
2024-10-24 08:34:22 -0700

    working on home page

---
+++ app/app/components/Info.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div className='flex flex-col justify-center items-center gap-6 '>
+      <div id='image' className='hover:shadow-2xl rounded-full cursor-pointer transition duration-300 ease-in-out transform hover:scale-105'>
+        <img  width={200} src="/profile picture.png" alt="image" />
+      </div>
+      <div id='description' className='w-1/2 text-lg text-slate-600'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis aliquam eum aut harum earum adipisci a vitae doloremque voluptatum facere, doloribus amet quo impedit, accusamus, ut culpa voluptatibus accusantium voluptas! Repellat, et.</p>
+      </div>
+      <ul id='socials' className='flex  items-center gap-6'>
+        <li className='hover:shadow-2xl rounded-full cursor-pointer transition duration-300 ease-in-out transform hover:scale-110'>
+          <img width={35} src="/Linkedin.png" alt="Linkedin"  />
+        </li>
+        <li className='hover:shadow-2xl rounded-full cursor-pointer transition duration-300 ease-in-out transform hover:scale-110'>
+          <img width={35} src="/Github.png" alt="Github"  />
+        </li>
+        <li className='hover:shadow-2xl rounded-full cursor-pointer transition duration-300 ease-in-out transform hover:scale-110'>
+          <img width={40} src="/Instagram.png" alt="Instagram"  />
+        </li>
+        <li className='hover:shadow-2xl rounded-full cursor-pointer transition duration-300 ease-in-out transform hover:scale-110'>
+          <img width={35} src="/Facebook.png" alt="Facebook"  />
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Info
